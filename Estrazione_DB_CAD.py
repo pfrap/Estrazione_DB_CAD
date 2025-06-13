ffb88e84d18adb1bcaa62798d518092a0b6fb621
Pietro <124562706+pfrap@users.noreply.github.com>
2025-06-13 12:35:03 +0200

    Update Estrazione_DB_CAD.py

--- Estrazione_DB_CAD.py
+++ Estrazione_DB_CAD.py
@@ -2,12 +2,10 @@ import streamlit as st
 import pandas as pd
 import io
 
-st.set_page_config(layout="wide")
-
 st.title("Estrazione DB CAD da CSV Autocad")
 
 # Layout a colonne
-col1, col2,col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
     st.header("Carica il file")
@@ -20,8 +18,8 @@ with col2:
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
 
-    st.subheader("Estrazione originale")
-    st.dataframe(df)
+    st.subheader("Anteprima dati originali")
+    st.dataframe(df.head())
 
     # Rinomina colonne se presenti
     if "Nome" in df.columns:
@@ -79,7 +77,6 @@ if uploaded_file is not None:
     # Ordina righe
     prod_df.sort_values(by=["GRUPPO", "TIP.COM", "A.N.", "HGT", "L.TOT.", "L.1"], inplace=True)
 
-with col3:
     st.subheader("Anteprima dati elaborati")
     st.dataframe(prod_df)
 
